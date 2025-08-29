bbc8c0c6a4865143e291f12e91c2a66a77e30221
onlinecalculatorpro-astro <onlinecalculatorpro@gmail.com>
2025-08-29 13:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, render_template_string
 from datetime import datetime, timezone
 import pytz
 from professional_astro import ProfessionalAstrologyEngine
